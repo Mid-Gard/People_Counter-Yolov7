ba65cd21a06ab44a6496a151b549152522b85fbf
tu2-atmanand <sanketgauns8@gmail.com>
2023-09-12 13:06:27 +0530

    Testing Command Updated

--- Testing.py
+++ Testing.py
@@ -1,3 +1,10 @@
+# Run the Following code in terminal to start the main code :
+# python yolov7/detect.py --weights model/yolov7-tiny.pt --conf 0.25 --img-size 640 --source http://192.168.29.188:8080/video?type=some.mjpeg --class 0
+
+
+# TODO : 1. Once you removed all the unnecessary stuff you can bring all the code from yolov7 outside or you can keep it inside and change the folder name to may be  core and run the above command from root folder.
+
+
 # from notebook_utils import download_file
 # from .openvino_notebooks.notebooks.utils.notebook_utils import download_file
 # import openvino_notebooks.notebooks.utils.notebook_utils as note
@@ -20,9 +27,6 @@ DATA_DIR.mkdir(exist_ok=True)
 # !python - W ignore yolov7/detect.py - -weights model/yolov7-tiny.pt - -conf 0.25 - -img-size 640 - -source yolov7/inference/images/horses.jpg
 
 
-
-
-
 # To test if the device is connected to your pc or not :
 
 #from openvino.runtime import Core
@@ -31,7 +35,7 @@ DATA_DIR.mkdir(exist_ok=True)
 
 #devices = core.available_devices
 
-#for device in devices:
+# for device in devices:
 #    device_name = core.get_property(device, "FULL_DEVICE_NAME")
 #    print(f"{device}: {device_name}")
 
